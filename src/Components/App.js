fe680f6f868fdfa95e50e49ff055ca5fc8abcbce
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2024-08-05 22:57:01 +0530

    RenderList contact-app

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,30 @@
+import  React from 'react'
+import Header from "./Header.js";
+import ContactList from "./ContactList.js";
+import AddContact from './AddContact.js';
 
-import './App.css';
+// import "./App.css";
 
 function App() {
+
+  const contacts =[
+    {
+      id:"1",
+      name:"Praveen",
+      email:"praveen@gmail.com",
+    },
+    {
+      id:"1",
+      name:"Ram",
+      email:"Ram@gmail.com",
+    }
+  ];
   return (
-  <div> Helo World</div>
+  <div> 
+  <Header />
+    <AddContact />
+   <ContactList contacts={contacts} />
+  </div>
   );
 }
 
